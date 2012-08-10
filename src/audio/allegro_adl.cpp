e261313f7bd4369cf3b4fb853fcdc199eec10701
Peter Wang <novalazy@gmail.com>
2012-08-11 09:55:59 +1000

    Add ADL player program using Allegro audio.

--- src/audio/allegro_adl.cpp
+++ src/audio/allegro_adl.cpp
@@ -1,8 +1,72 @@
 #include <allegro5/allegro.h>
 #include <allegro5/allegro_audio.h>
 
-int main(void)
+#include "adl/sound_adlib.h"
+
+const int SRATE     = 48000;
+const int NUMFRAGS  = 4;
+const int FRAGLEN   = 2048;
+
+int main(int argc, const char **argv)
 {
+    ALLEGRO_AUDIO_STREAM *stream;
+    ALLEGRO_EVENT_QUEUE *queue;
+    ALLEGRO_EVENT ev;
+    ALLEGRO_FILE *f;
+    int track;
+    int mame;
+
+    if (argc < 4) {
+        return 1;
+    }
+    if (!al_init()) {
+        return 1;
+    }
+    if (!al_install_audio()) {
+        return 1;
+    }
+    if (!al_reserve_samples(1)) {
+        return 1;
+    }
+
+    f = al_fopen(argv[1], "rb");
+    if (!f) {
+        return 1;
+    }
+
+    track = atoi(argv[2]);
+
+    mame = atoi(argv[3]);
+
+    SoundAdlibPC adlib(f, SRATE, mame);
+
+    al_fclose(f);
+
+    stream = al_create_audio_stream(NUMFRAGS, FRAGLEN, SRATE,
+        ALLEGRO_AUDIO_DEPTH_INT16, ALLEGRO_CHANNEL_CONF_1);
+    if (!stream) {
+        return 1;
+    }
+    al_attach_audio_stream_to_mixer(stream, al_get_default_mixer());
+
+    queue = al_create_event_queue();
+    al_register_event_source(queue, al_get_audio_stream_event_source(stream));
+
+    adlib.playTrack(track);
+
+    for (;;) {
+        al_wait_for_event(queue, &ev);
+
+        if (ev.type == ALLEGRO_EVENT_AUDIO_STREAM_FRAGMENT) {
+            void *frag = al_get_audio_stream_fragment(stream);
+
+            adlib.callback(&adlib, (SoundAdlibPC::Uint8 *)frag,
+                FRAGLEN * al_get_audio_depth_size(ALLEGRO_AUDIO_DEPTH_INT16));
+
+            al_set_audio_stream_fragment(stream, frag);
+        }
+    }
+
     return 0;
 }
 
