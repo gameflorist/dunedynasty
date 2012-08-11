5bc1b32fa4f1ff3db7967bc9d311b1a78407b0d6
Peter Wang <novalazy@gmail.com>
2012-08-11 11:27:26 +1000

    Load XMIDI file.

--- src/audio/allegro_midi.cpp
+++ src/audio/allegro_midi.cpp
@@ -2,8 +2,38 @@
 #include <allegro5/allegro_audio.h>
 #include <fluidsynth.h>
 
+#include "xmi/xmidi.h"
+
 int main(int argc, const char **argv)
 {
+    const char *filename;
+    int track;
+    FILE *f;
+
+    if (argc < 3) {
+        return 1;
+    }
+
+    filename = argv[1];
+    track = atoi(argv[2]);
+
+    f = fopen(filename, "rb");
+    if (!f) {
+        return 1;
+    }
+
+    FileDataSource input(f);
+
+    XMIDI xmidi(&input, XMIDI_CONVERT_NOCONVERSION);
+
+    fclose(f);
+
+    midi_event *midi_events;
+    int ppqn;
+    if (!xmidi.retrieve(track, &midi_events, ppqn)) {
+        return 1;
+    }
+
     return 0;
 }
 
