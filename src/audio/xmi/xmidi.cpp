89ea35f538a8898256bc0a19c9fb08ec9bb8d94d
Peter Wang <novalazy@gmail.com>
2012-08-11 11:04:14 +1000

    Adjust include paths.

--- src/audio/xmi/xmidi.cpp
+++ src/audio/xmi/xmidi.cpp
@@ -21,7 +21,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include <stdio.h>
 #include <iostream>
 //#include "randgen.h"
-#include <FileClasses/xmidi/xmidi.h>
+#include "xmidi.h"
 
 using namespace std;
 
