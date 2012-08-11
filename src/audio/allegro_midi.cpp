c687dc2a3cc98e42a4737f579801d2317f69760a
Peter Wang <novalazy@gmail.com>
2012-08-11 11:11:51 +1000

    Add stub for MIDI player using Allegro/fluidsynth.

---
+++ src/audio/allegro_midi.cpp
@@ -0,0 +1,10 @@
+#include <allegro5/allegro.h>
+#include <allegro5/allegro_audio.h>
+#include <fluidsynth.h>
+
+int main(int argc, const char **argv)
+{
+    return 0;
+}
+
+/* vim: set sts=4 sw=4 et: */
