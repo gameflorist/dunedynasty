83e059c162964586e9015801c27f2a4fc8384328
David Wang <millimillenary@gmail.com>
2012-08-16 17:22:15 +1000

    Draw sidebar to fill screen height.

--- src/newui/menubar.c
+++ src/newui/menubar.c
@@ -10,6 +10,10 @@
 void
 MenuBar_Draw(enum HouseType houseID)
 {
+	for (int y = TRUE_DISPLAY_HEIGHT - 83 - 52; y + 52 - 1 >= 40 + 17; y -= 52) {
+		Video_DrawCPSSpecial(CPS_SIDEBAR_MIDDLE, houseID, TRUE_DISPLAY_WIDTH - 80, y);
+	}
+
 	for (int x = TRUE_DISPLAY_WIDTH - 136 - 320; x + 320 - 1 >= 184; x -= 320) {
 		Video_DrawCPSSpecial(CPS_MENUBAR_MIDDLE, houseID, x, 0);
 	}
@@ -22,4 +26,7 @@ MenuBar_Draw(enum HouseType houseID)
 	Video_DrawCPSSpecial(CPS_MENUBAR_RIGHT, houseID, TRUE_DISPLAY_WIDTH - 136, 0);
 	Video_DrawCPSSpecial(CPS_STATUSBAR_LEFT, houseID, 0, 17);
 	Video_DrawCPSSpecial(CPS_STATUSBAR_RIGHT, houseID, TRUE_DISPLAY_WIDTH - 8, 17);
+	Video_DrawCPSSpecial(CPS_SIDEBAR_TOP, houseID, TRUE_DISPLAY_WIDTH - 80, 40);
+	Video_DrawCPSSpecial(CPS_SIDEBAR_BOTTOM, houseID, TRUE_DISPLAY_WIDTH - 80, TRUE_DISPLAY_HEIGHT - 83);
+	GUI_DrawFilledRectangle(TRUE_DISPLAY_WIDTH - 64, TRUE_DISPLAY_HEIGHT - 64, TRUE_DISPLAY_WIDTH, TRUE_DISPLAY_HEIGHT, 0);
 }
