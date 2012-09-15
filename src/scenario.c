df378e608ad4fdc3a5e33a47ebe64ac8a68bd62e
David Wang <millimillenary@gmail.com>
2012-09-15 15:02:27 +1000

    Fix scenario typos, giving Atreides a useless WOR trooper facility.

--- src/scenario.c
+++ src/scenario.c
@@ -234,6 +234,12 @@ static void Scenario_Load_Structure(const char *key, char *settings)
 		structureType = Structure_StringToType(settings);
 		if (structureType == STRUCTURE_INVALID) return;
 
+		/* ENHANCEMENT -- Atreides shouldn't get WOR since they can't do anything with it. */
+		if (enhancement_fix_scenario_typos) {
+			if (houseType == HOUSE_ATREIDES && structureType == STRUCTURE_WOR_TROOPER)
+				structureType = STRUCTURE_BARRACKS;
+		}
+
 		Structure_Create(STRUCTURE_INDEX_INVALID, structureType, houseType, position);
 		return;
 	}
