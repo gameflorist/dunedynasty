05865c453fc790ad0d65d58fcf68637def044941
David Wang <millimillenary@gmail.com>
2012-12-27 10:38:19 +1100

    Skirmish: divide maps into islands of rock.
    Add debug code to visualise remaining buildable tiles.

--- src/mapgenerator/skirmish.c
+++ src/mapgenerator/skirmish.c
@@ -2,28 +2,216 @@
 
 #include <assert.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "skirmish.h"
 
 #include "../map.h"
+#include "../opendune.h"
 #include "../scenario.h"
+#include "../sprites.h"
+#include "../tile.h"
 #include "../tools.h"
+
+typedef struct {
+	int x, y;
+	uint16 packed;
+	int parent;
+} BuildableTile;
+
+typedef struct {
+	int start;
+	int end;
+	bool used;
+} Island;
+
+typedef struct {
+	/* List of buildable tiles. */
+	BuildableTile buildable[MAP_SIZE_MAX * MAP_SIZE_MAX];
+
+	/* Packed index -> island index. */
+	int islandID[MAP_SIZE_MAX * MAP_SIZE_MAX];
+
+	int nislands;
+	int nislands_unused;
+	Island *island;
+} SkirmishData;
+
+/*--------------------------------------------------------------*/
+
+static void
+Skirmish_GenGeneral(void)
+{
+	memset(&g_scenario, 0, sizeof(Scenario));
+	g_scenario.winFlags = 3;
+	g_scenario.loseFlags = 1;
+	g_scenario.mapSeed = g_skirmish.seed;
+	g_scenario.mapScale = 0;
+	g_scenario.timeOut = 0;
+}
+
+/* Use breadth first flood fill to create a list of buildable tiles
+ * around x0, y0.  Only fill tiles that are part of the same source
+ * island (similar to bucket fill same colour).
+ */
+static int
+Skirmish_FindBuildableArea(int island, int x0, int y0,
+		SkirmishData *sd, BuildableTile *buildable)
+{
+	const int dx[4] = {  0, 1, 0, -1 };
+	const int dy[4] = { -1, 0, 1,  0 };
+	int *islandID = sd->islandID;
+	int n = 0;
+
+	/* Starting case. */
+	do {
+		if (!(Map_InRangeX(x0) && Map_InRangeY(y0)))
+			break;
+
+		const uint16 packed = Tile_PackXY(x0, y0);
+		if (islandID[packed] != island)
+			break;
+
+		const enum LandscapeType lst = Map_GetLandscapeType(packed);
+		const LandscapeInfo *li = &g_table_landscapeInfo[lst];
+		if (!(li->isValidForStructure || li->isValidForStructure2))
+			break;
+
+		buildable[n].x = x0;
+		buildable[n].y = y0;
+		buildable[n].packed = packed;
+		buildable[n].parent = 0;
+		islandID[packed] = sd->nislands;
+		n++;
+	} while (false);
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < 4; j++) {
+			const int x = buildable[i].x + dx[j];
+			const int y = buildable[i].y + dy[j];
+			if (!(Map_InRangeX(x) && Map_InRangeY(y)))
+				continue;
+
+			const uint16 packed = Tile_PackXY(x, y);
+			if (islandID[packed] != island)
+				continue;
+
+			const enum LandscapeType lst = Map_GetLandscapeType(packed);
+			const LandscapeInfo *li = &g_table_landscapeInfo[lst];
+			if (!(li->isValidForStructure || li->isValidForStructure2))
+				continue;
+
+			buildable[n].x = x;
+			buildable[n].y = y;
+			buildable[n].packed = packed;
+			buildable[n].parent = i;
+			islandID[packed] = sd->nislands;
+			n++;
+		}
+	}
+
+	return n;
+}
+
+static void
+Skirmish_DivideIsland(int island, SkirmishData *sd)
+{
+	int start = sd->island[island].start;
+	const int len = sd->island[island].end - sd->island[island].start;
+	BuildableTile *orig = malloc(len * sizeof(orig[0]));
+	memcpy(orig, sd->buildable + start, len * sizeof(orig[0]));
+
+	for (int i = 0; i < len; i++) {
+		const int area = Skirmish_FindBuildableArea(island, orig[i].x, orig[i].y, sd, sd->buildable + start);
+
+		if (area >= 50) {
+			sd->island = realloc(sd->island, (sd->nislands + 1) * sizeof(sd->island[0]));
+			assert(sd->island != NULL);
+
+			sd->island[sd->nislands].start = start;
+			sd->island[sd->nislands].end = start + area;
+			sd->island[sd->nislands].used = false;
+
+			start += area;
+			sd->nislands++;
+			sd->nislands_unused++;
+		}
+	}
+
+	sd->island[island].used = true;
+	sd->nislands_unused--;
+	free(orig);
+}
+
+static bool
+Skirmish_GenerateMapInner(SkirmishData *sd)
+{
+	const MapInfo *mi = &g_mapInfos[0];
+
+	Game_Init();
+
+	Skirmish_GenGeneral();
+	Sprites_LoadTiles();
+	Tools_RandomLCG_Seed(g_skirmish.seed);
+	Map_CreateLandscape(g_skirmish.seed);
+
+	/* Create initial island. */
+	sd->island[0].start = 0;
+	sd->island[0].end = 0;
+	for (int dy = 0; dy < mi->sizeY; dy++) {
+		for (int dx = 0; dx < mi->sizeX; dx++) {
+			const int tx = mi->minX + dx;
+			const int ty = mi->minY + dy;
+			const uint16 packed = Tile_PackXY(tx, ty);
+
+			sd->buildable[sd->island[0].end].x = tx;
+			sd->buildable[sd->island[0].end].y = ty;
+			sd->buildable[sd->island[0].end].packed = packed;
+			sd->buildable[sd->island[0].end].parent = 0;
+			sd->island[0].end++;
+		}
+	}
+
+	memset(sd->islandID, 0, MAP_SIZE_MAX * MAP_SIZE_MAX * sizeof(sd->islandID[0]));
+	Skirmish_DivideIsland(0, sd);
+
+	if (sd->nislands_unused == 0)
+		return false;
+
+#if 0
+	/* Debugging. */
+	for (int island = 0; island < sd.nislands; island++) {
+		if (sd.island[island].used)
+			continue;
+
+		for (int i = sd.island[island].start; i < sd.island[island].end; i++) {
+			g_map[sd.buildable[i].packed].groundSpriteID = g_veiledSpriteID;
+		}
+	}
+#endif
+
+	Game_Prepare();
+	return true;
+}
 
 bool
 Skirmish_GenerateMap(bool newseed)
 {
-	uint32 seed;
-
 	if (newseed) {
 		/* DuneMaps only supports 15 bit maps seeds, so there. */
-		seed = rand() & 0x7FFF;
-		g_skirmish.seed = seed;
-	}
-	else {
-		seed = g_skirmish.seed;
+		g_skirmish.seed = rand() & 0x7FFF;
 	}
 
-	Tools_RandomLCG_Seed(seed);
-	Map_CreateLandscape(seed);
-	return true;
+	SkirmishData sd;
+
+	sd.island = malloc(sizeof(sd.island[0]));
+	assert(sd.island != NULL);
+
+	sd.nislands = 1;
+	sd.nislands_unused = 1;
+
+	const bool ret = Skirmish_GenerateMapInner(&sd);
+
+	free(sd.island);
+	return ret;
 }
