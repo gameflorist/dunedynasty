2883cbb9809772c8fac05027d3de1b778088109c
David Wang <millimillenary@gmail.com>
2013-07-06 13:21:13 +1000

    Annotate xorshift RNG.

--- src/tools/random_xorshift.c
+++ src/tools/random_xorshift.c
@@ -16,6 +16,10 @@ typedef struct xorshift {
 
 static xorshift_t s_xorshift;
 
+/**
+ * @brief   Reseeds the xorshift state.
+ * @details http://en.wikipedia.org/wiki/Xorshift
+ */
 void
 Random_Xorshift_Seed(uint32 x, uint32 y, uint32 z, uint32 w)
 {
@@ -34,6 +38,10 @@ Random_Xorshift_Seed(uint32 x, uint32 y, uint32 z, uint32 w)
 	s->w = w;
 }
 
+/**
+ * @brief   Generate a random number.
+ * @details http://en.wikipedia.org/wiki/Xorshift
+ */
 static uint32
 xor128(void)
 {
@@ -47,12 +55,20 @@ xor128(void)
 	return s->w = s->w ^ (s->w >> 19) ^ (t ^ (t >> 8));
 }
 
+/**
+ * @brief   Tools_Random_256, for non-game-mechanics.
+ * @details @see Tools_Random_256.
+ */
 uint8
 Random_Xorshift_256(void)
 {
 	return xor128() & 0xFF;
 }
 
+/**
+ * @brief   Tools_RandomLCG_Range, for non-game-mechanics.
+ * @details @see Tools_RandomLCG_Range.
+ */
 uint16
 Random_Xorshift_Range(uint16 min, uint16 max)
 {
