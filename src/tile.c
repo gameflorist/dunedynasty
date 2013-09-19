a6fff2d73c4ab2566721975eb2b36eb0fb4d06c6
David Wang <millimillenary@gmail.com>
2013-09-19 20:54:53 +1000

    Fog of war: refresh fog of war for non-local players.

--- src/tile.c
+++ src/tile.c
@@ -6,45 +6,50 @@
 #include "map.h"
 #include "tools/coord.h"
 
-/**
- * Remove fog in the radius around the given tile.
- *
- * @param tile The tile to remove fog around.
- * @param radius The radius to remove fog around.
- */
+static void
+Map_UnveilTileForHouses(enum HouseFlag houses, uint16 packed)
+{
+	for (enum HouseType h = HOUSE_HARKONNEN; h < HOUSE_MAX; h++) {
+		if (houses & (1 << h))
+			Map_UnveilTile(packed, h);
+	}
+}
+
 void
 Tile_RefreshFogInRadius(enum HouseFlag houses,
 		tile32 tile, uint16 radius, bool unveil)
 {
-	uint16 packed;
-	uint16 x, y;
-	int16 i, j;
-
-	if (!(houses & (1 << g_playerHouseID)))
+	uint16 packed = Tile_PackTile(tile);
+	if (!Map_IsValidPosition(packed))
 		return;
 
-	packed = Tile_PackTile(tile);
+	for (enum HouseType h = HOUSE_HARKONNEN; h < HOUSE_MAX; h++) {
+		if (!House_IsHuman(h))
+			houses &= ~(1 << h);
+	}
 
-	if (!Map_IsValidPosition(packed)) return;
+	if (houses == 0)
+		return;
 
-	x = Tile_GetPackedX(packed);
-	y = Tile_GetPackedY(packed);
+	const int x = Tile_GetPackedX(packed);
+	const int y = Tile_GetPackedY(packed);
 	tile = Tile_MakeXY(x, y);
 
-	for (i = -radius; i <= radius; i++) {
-		for (j = -radius; j <= radius; j++) {
-			tile32 t;
+	for (int i = -radius; i <= radius; i++) {
+		if (!(0 <= (x + i) && (x + i) < MAP_SIZE_MAX))
+			continue;
 
-			if ((x + i) < 0 || (x + i) >= 64) continue;
-			if ((y + j) < 0 || (y + j) >= 64) continue;
+		for (int j = -radius; j <= radius; j++) {
+			if (!(0 <= (y + j) && (y + j) < MAP_SIZE_MAX))
+				continue;
+
+			const tile32 t = Tile_MakeXY(x + i, y + j);
+			if (Tile_GetDistanceRoundedUp(tile, t) > radius)
+				continue;
 
 			packed = Tile_PackXY(x + i, y + j);
-			t = Tile_MakeXY(x + i, y + j);
-
-			if (Tile_GetDistanceRoundedUp(tile, t) > radius) continue;
-
 			if (unveil) {
-				Map_UnveilTile(packed, g_playerHouseID);
+				Map_UnveilTileForHouses(houses, packed);
 			}
 			else if (houses & (1 << g_playerHouseID)) {
 				Map_Client_RefreshTile(packed, 2);
