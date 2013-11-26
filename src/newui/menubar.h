964b1444205f4ac63e72305bac4cd0401593ef95
David Wang <millimillenary@gmail.com>
2013-11-27 09:01:19 +1100

    Forward declare widget structure instead of including entire file.

--- src/newui/menubar.h
+++ src/newui/menubar.h
@@ -3,8 +3,9 @@
 
 #include "enum_string.h"
 #include "enumeration.h"
-#include "../gui/widget.h"
 #include "../shape.h"
+
+struct Widget;
 
 extern void MenuBar_DrawCredits(int credits_new, int credits_old, int offset, int x0);
 extern void MenuBar_DrawStatusBar(const char *line1, const char *line2, bool scrollInProgress, int x, int y, int offset);
@@ -15,11 +16,11 @@ extern void GUI_DisplayHint(enum HouseType houseID, enum StringID stringID, enum
 extern void MenuBar_TickHintOverlay(void);
 extern void MenuBar_DrawHintOverlay(void);
 
-extern bool MenuBar_ClickMentat(Widget *w);
+extern bool MenuBar_ClickMentat(struct Widget *w);
 extern void MenuBar_TickMentatOverlay(void);
 extern void MenuBar_DrawMentatOverlay(void);
 
-extern bool MenuBar_ClickOptions(Widget *w);
+extern bool MenuBar_ClickOptions(struct Widget *w);
 extern void MenuBar_TickOptionsOverlay(void);
 extern void MenuBar_DrawOptionsOverlay(void);
 
