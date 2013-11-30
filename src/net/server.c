gameflorist/dunedynasty

commit 90c3ce757501466da3d55919611e256cee8bb880

C2S: starport orders, and create units when purchased.
Units are now created as soon as they are queued to avoid the need to
refund units later.  This is more like the CHOAM screen behaviour.

src/net/server.c
@@ -2,6 +2,7 @@
 
 #include <assert.h>
 #include <stdio.h>
+#include <string.h>
 #include "enum_string.h"
 
 #include "server.h"
@@ -9,6 +10,7 @@
 #include "message.h"
 #include "../audio/audio.h"
 #include "../enhancement.h"
+#include "../newui/actionpanel.h"
 #include "../gui/gui.h"
 #include "../house.h"
 #include "../newui/viewport.h"
@@ -21,6 +23,7 @@
 #include "../structure.h"
 #include "../tools/coord.h"
 #include "../tools/encoded_index.h"
+#include "../tools/random_starport.h"
 #include "../unit.h"
 
 #if 0
@@ -159,6 +162,104 @@ Server_Recv_CancelItem(Structure *s)
 }
 
 static void
+Server_Recv_PurchaseItemStarport(Structure *s, uint8 objectType)
+{
+	if (objectType >= UNIT_MAX || g_starportAvailable[objectType] <= 0)
+		return;
+
+	const int credits = Random_Starport_CalculateUnitPrice(objectType);
+	House *h = House_Get_ByIndex(s->o.houseID);
+	if (h->credits <= credits)
+		return;
+
+	/* Attempt to create a unit. */
+	Unit *u;
+	g_validateStrictIfZero++;
+	{
+		tile32 tile;
+		tile.x = 0xFFFF;
+		tile.y = 0xFFFF;
+		u = Unit_Create(UNIT_INDEX_INVALID, objectType, s->o.houseID, tile, 0);
+	}
+	g_validateStrictIfZero--;
+
+	if (u == NULL) {
+		Server_Send_StatusMessage1(1 << h->index, 2,
+				STR_UNABLE_TO_CREATE_MORE);
+		Server_Send_PlaySound(1 << h->index, EFFECT_ERROR_OCCURRED);
+	}
+	else {
+		h->credits -= credits;
+		u->o.linkedID = h->starportLinkedID & 0xFF;
+		h->starportLinkedID = u->o.index;
+
+		h->starportCount[objectType]++;
+		BuildQueue_Add(&h->starportQueue, objectType, credits);
+
+		g_starportAvailable[objectType]--;
+		if (g_starportAvailable[objectType] <= 0)
+			g_starportAvailable[objectType] = -1;
+	}
+}
+
+static void
+Server_Recv_CancelItemStarport(Structure *s, uint8 objectType)
+{
+	if (g_starportAvailable[objectType] == 0)
+		return;
+
+	House *h = House_Get_ByIndex(s->o.houseID);
+
+	int credits;
+	if (BuildQueue_RemoveTail(&h->starportQueue, objectType, &credits)) {
+		h->credits += credits;
+		h->starportCount[objectType]--;
+		Structure_Server_RestockStarport(objectType);
+
+		/* We create units as soon as they are purchased (due to unit limits),
+		 * so we need to free them too!
+		 */
+		uint8 *prev = NULL;
+		uint16 unitID = h->starportLinkedID;
+		while (unitID != 0xFF) {
+			Unit *u = Unit_Get_ByIndex(unitID);
+
+			if (u->o.type == objectType) {
+				if (prev == NULL) {
+					h->starportLinkedID
+						= (u->o.linkedID == 0xFF)
+						? UNIT_INDEX_INVALID : u->o.linkedID;
+				}
+				else {
+					*prev = u->o.linkedID;
+				}
+
+				Unit_Free(u);
+				break;
+			}
+
+			prev = &u->o.linkedID;
+			unitID = u->o.linkedID;
+		}
+	}
+}
+
+static void
+Server_Recv_SendStarportOrder(Structure *s)
+{
+	House *h = House_Get_ByIndex(s->o.houseID);
+
+	if (BuildQueue_IsEmpty(&h->starportQueue))
+		return;
+
+	if (h->starportTimeLeft == 0) {
+		h->starportTimeLeft = g_table_houseInfo[h->index].starportDeliveryTime;
+		memset(h->starportCount, 0, sizeof(h->starportCount));
+		BuildQueue_Free(&h->starportQueue);
+	}
+}
+
+static void
 Server_Recv_PurchaseResumeItem(enum HouseType houseID, const unsigned char *buf)
 {
 	const uint16 objectID   = Net_Decode_ObjectIndex(&buf);
@@ -172,6 +273,11 @@ Server_Recv_PurchaseResumeItem(enum HouseType houseID, const unsigned char *buf)
 	Structure *s = Structure_Get_ByIndex(objectID);
 	if (!Server_PlayerCanControlStructure(houseID, s))
 		return;
+
+	if (s->o.type == STRUCTURE_STARPORT) {
+		Server_Recv_PurchaseItemStarport(s, objectType);
+		return;
+	}
 
 	if (objectType == 0xFF) {
 		Server_Recv_CancelItem(s);
@@ -221,11 +327,13 @@ Server_Recv_PauseCancelItem(enum HouseType houseID, const unsigned char *buf)
 		return;
 
 	Structure *s = Structure_Get_ByIndex(objectID);
-	if (!Server_PlayerCanControlStructure(houseID, s)
-			|| s->o.linkedID == 0xFF)
+	if (!Server_PlayerCanControlStructure(houseID, s))
 		return;
 
-	if (s->objectType == objectType) {
+	if (s->o.type == STRUCTURE_STARPORT) {
+		Server_Recv_CancelItemStarport(s, objectType);
+	}
+	else if (s->objectType == objectType && s->o.linkedID != 0xFF) {
 		if (s->o.flags.s.onHold) {
 			Server_Recv_CancelItem(s);
 		}
@@ -310,6 +418,9 @@ Server_Recv_ActivateStructureAbility(enum HouseType houseID, const unsigned char
 	if (s->o.type == STRUCTURE_PALACE) {
 		if (s->countDown == 0)
 			Structure_Server_ActivateSpecial(s);
+	}
+	else if (s->o.type == STRUCTURE_STARPORT) {
+		Server_Recv_SendStarportOrder(s);
 	}
 	else if (s->o.type == STRUCTURE_REPAIR) {
 		if (s->o.linkedID != 0xFF)

src/gui/widget_click.c
@@ -46,14 +46,7 @@ bool GUI_Widget_SpriteTextButton_Click(Widget *w)
 		return false;
 
 	if (s->o.type == STRUCTURE_STARPORT) {
-		const House *h = House_Get_ByIndex(s->o.houseID);
-
-		if (h->starportLinkedID == 0xFFFF) {
-			return ActionPanel_ClickStarport(w, s);
-		}
-		else {
-			return false;
-		}
+		return ActionPanel_ClickStarport(w, s);
 	}
 
 	switch (g_productionStringID) {
@@ -207,7 +200,7 @@ GUI_Widget_Picture_Click(Widget *w)
 {
 	VARIABLE_NOT_USED(w);
 
-	Structure *s = Structure_Get_ByPackedTile(g_selectionPosition);
+	const Structure *s = Structure_Get_ByPackedTile(g_selectionPosition);
 	if (s == NULL)
 		return false;
 
@@ -219,8 +212,9 @@ GUI_Widget_Picture_Click(Widget *w)
 		if (s->countDown == 0)
 			Client_Send_ActivateSuperweapon(&s->o);
 	}
-	else if ((s->o.type == STRUCTURE_STARPORT) && (!BuildQueue_IsEmpty(&g_playerHouse->starportQueue))) {
-		ActionPanel_ClickStarportOrder(s);
+	else if (s->o.type == STRUCTURE_STARPORT) {
+		if (!House_StarportQueueEmpty(g_playerHouse))
+			Client_Send_SendStarportOrder(&s->o);
 	}
 	else if (s->o.type == STRUCTURE_REPAIR) {
 		if (s->o.linkedID != 0xFF)

src/gui/widget_draw.c
@@ -443,19 +443,13 @@ void GUI_Widget_ActionPanel_Draw(bool forceDraw)
 
 		switch (actionType) {
 			case 3: /* Structure */
-#if 0
-				if (oi->flags.factory && !isNotPlayerOwned) {
-					GUI_Widget_MakeVisible(widget28);
-				}
-#else
 				if (!isNotPlayerOwned &&
 						((g_productionStringID == STR_PLACE_IT) ||
 						 (s->o.type == STRUCTURE_PALACE && s->countDown == 0) ||
-						 (s->o.type == STRUCTURE_STARPORT && !BuildQueue_IsEmpty(&h->starportQueue)) ||
+						 (s->o.type == STRUCTURE_STARPORT && !House_StarportQueueEmpty(h)) ||
 						 (s->o.type == STRUCTURE_REPAIR && s->o.linkedID != 0xFF))) {
 					GUI_Widget_MakeVisible(widget28);
 				}
-#endif
 				/* Fall through */
 			case 7: /* Placement */
 			case 2: /* Unit */
@@ -550,9 +544,10 @@ void GUI_Widget_ActionPanel_Draw(bool forceDraw)
 						/* GUI_Widget_MakeNormal(widget24, false); */
 					}
 
-					if ((oi->flags.factory && o->type != STRUCTURE_STARPORT) ||
-					    (o->type == STRUCTURE_STARPORT && (h->starportLinkedID == 0xFFFF)) ||
-					    (o->type == STRUCTURE_PALACE && s->countDown == 0)) {
+					if ((o->type == STRUCTURE_PALACE && s->countDown == 0)
+					 || (o->type != STRUCTURE_STARPORT && oi->flags.factory)
+					 || (o->type == STRUCTURE_STARPORT
+						 && (h->starportLinkedID == UNIT_INDEX_INVALID || !House_StarportQueueEmpty(h)))) {
 						GUI_Widget_MakeVisible(widget2C);
 					}
 

src/house.c
@@ -131,7 +131,7 @@ void GameLoop_House(void)
 		 * bug; that's just how the game is.
 		 */
 		const enum UnitType type = Tools_RandomLCG_Range(0, UNIT_MAX - 1);
-		Structure_Starport_Restock(type);
+		Structure_Server_RestockStarport(type);
 		g_factoryWindowTotal = -1;
 	}
 
@@ -233,11 +233,17 @@ void GameLoop_House(void)
 
 		if (tickHouse) House_EnsureHarvesterAvailable(h->index);
 
-		/* ENHANCEMENT -- If no starports remaining, create a
+		/* If h->starportLinkedID != UNIT_INDEX_INVALID and the queue
+		 * is empty, that means we are waiting for the countdown
+		 * before creating a frigate and delivering the units.
+		 *
+		 * ENHANCEMENT -- If no starports remaining, create a
 		 * reinforcement carryall to reliably drop off the ordered
 		 * units (at the home base).
 		 */
-		if (tickStarport && h->starportLinkedID != UNIT_INDEX_INVALID) {
+		if (tickStarport
+				&& h->starportLinkedID != UNIT_INDEX_INVALID
+				&& BuildQueue_IsEmpty(&h->starportQueue)) {
 
 			h->starportTimeLeft--;
 			if ((int16)h->starportTimeLeft < 0) h->starportTimeLeft = 0;
@@ -534,6 +540,17 @@ void House_CalculatePowerAndCredit(House *h)
 		if (g_scenario.structuresLost[h->index] > 0)
 			h->creditsStorageNoSilo = 0;
 	}
+}
+
+bool
+House_StarportQueueEmpty(const House *h)
+{
+	for (enum UnitType u = UNIT_CARRYALL; u < UNIT_MAX; u++) {
+		if (h->starportCount[u] > 0)
+			return false;
+	}
+
+	return true;
 }
 
 const char *House_GetWSAHouseFilename(uint8 houseID)

src/house.h
@@ -70,6 +70,7 @@ typedef struct House {
 	uint16 starportID;
 	uint16 houseMissileID;
 	uint8  houseMissileCountdown;
+	uint8  starportCount[UNIT_MAX];
 	BuildQueue starportQueue;
 } House;
 
@@ -126,6 +127,7 @@ extern bool House_AreAllied(uint8 houseID1, uint8 houseID2);
 extern bool House_UpdateRadarState(House *h);
 extern void House_UpdateCreditsStorage(uint8 houseID);
 extern void House_CalculatePowerAndCredit(struct House *h);
+extern bool House_StarportQueueEmpty(const House *h);
 extern const char *House_GetWSAHouseFilename(uint8 houseID);
 
 extern enum UnitType House_GetInfantrySquad(enum HouseType houseID);

src/net/client.c
@@ -124,6 +124,12 @@ Client_Send_PlaceStructure(uint16 packed)
 }
 
 void
+Client_Send_SendStarportOrder(const struct Object *o)
+{
+	Client_Send_ObjectIndex(CSMSG_ACTIVATE_STRUCTURE_ABILITY, o);
+}
+
+void
 Client_Send_ActivateSuperweapon(const struct Object *o)
 {
 	Client_Send_ObjectIndex(CSMSG_ACTIVATE_STRUCTURE_ABILITY, o);

src/net/client.h
@@ -12,6 +12,7 @@ extern void Client_Send_PauseCancelItem(const struct Object *o, uint8 objectType
 extern void Client_Send_EnterPlacementMode(const struct Object *o);
 extern void Client_Send_LeavePlacementMode(const struct Object *o);
 extern void Client_Send_PlaceStructure(uint16 packed);
+extern void Client_Send_SendStarportOrder(const struct Object *o);
 extern void Client_Send_ActivateSuperweapon(const struct Object *o);
 extern void Client_Send_LaunchDeathhand(uint16 packed);
 extern void Client_Send_EjectRepairFacility(const struct Object *o);

src/newui/actionpanel.c
@@ -547,67 +547,13 @@ ActionPanel_ClickFactory(const Widget *widget, Structure *s)
 	return false;
 }
 
-void
-ActionPanel_ClickStarportOrder(Structure *s)
-{
-	House *h = g_playerHouse;
-
-	while (!BuildQueue_IsEmpty(&h->starportQueue)) {
-		int credits = h->starportQueue.first->credits;
-		uint16 objectType = BuildQueue_RemoveHead(&h->starportQueue);
-		Unit *u;
-
-		g_validateStrictIfZero++;
-		{
-			tile32 tile;
-			tile.x = 0xFFFF;
-			tile.y = 0xFFFF;
-			u = Unit_Create(UNIT_INDEX_INVALID, (uint8)objectType, s->o.houseID, tile, 0);
-		}
-		g_validateStrictIfZero--;
-
-		if (u == NULL) {
-			/* Originally the starport only allowed you to purchase a
-			 * unit if there was an empty index.  However, that only
-			 * really worked with the factory window interface.
-			 */
-			h->credits += credits;
-			Structure_Starport_Restock(objectType);
-
-			Server_Send_StatusMessage1(1 << s->o.houseID, 2,
-					STR_UNABLE_TO_CREATE_MORE);
-
-			continue;
-		}
-
-		h->starportID = s->o.index;
-
-		if (h->starportTimeLeft == 0) h->starportTimeLeft = g_table_houseInfo[h->index].starportDeliveryTime;
-
-		u->o.linkedID = h->starportLinkedID & 0xFF;
-		h->starportLinkedID = u->o.index;
-	}
-}
-
 static void
 ActionPanel_ClickStarportPlus(const Structure *s, int entry)
 {
 	const FactoryWindowItem *item = &g_factoryWindowItems[entry];
-	const uint16 type = item->objectType;
 
-	House *h = House_Get_ByIndex(s->o.houseID);
-
-	if ((g_starportAvailable[type] > 0) && (item->credits <= h->credits)) {
-		BuildQueue_Add(&h->starportQueue, item->objectType, item->credits);
-
-		if (g_starportAvailable[type] == 1) {
-			g_starportAvailable[type] = -1;
-		}
-		else {
-			g_starportAvailable[type]--;
-		}
-
-		h->credits -= item->credits;
+	if ((g_starportAvailable[item->objectType] > 0) && (item->credits <= g_playerHouse->credits)) {
+		Client_Send_PurchaseResumeItem(&s->o, item->objectType);
 	}
 	else {
 		Audio_PlaySound(EFFECT_ERROR_OCCURRED);
@@ -618,15 +564,9 @@ static void
 ActionPanel_ClickStarportMinus(const Structure *s, int entry)
 {
 	const FactoryWindowItem *item = &g_factoryWindowItems[entry];
-	const uint16 type = item->objectType;
 
-	House *h = House_Get_ByIndex(s->o.houseID);
-	int credits;
-
-	if (BuildQueue_RemoveTail(&h->starportQueue, type, &credits)) {
-		Structure_Starport_Restock(type);
-		h->credits += credits;
-	}
+	if (g_playerHouse->starportCount[item->objectType] > 0)
+		Client_Send_PauseCancelItem(&s->o, item->objectType);
 }
 
 bool
@@ -647,7 +587,8 @@ ActionPanel_ClickStarport(const Widget *widget, Structure *s)
 	if (s_factory_panel_layout != FACTORYPANEL_SMALL_ICONS_WITHOUT_SCROLL) {
 		ActionPanel_SendOrderButtonDimensions(widget, &x1, &y1, &x2, &y2, NULL, NULL);
 		if (lmb && Mouse_InRegion_Div(widget->div, x1, y1, x2, y2)) {
-			ActionPanel_ClickStarportOrder(s);
+			if (!House_StarportQueueEmpty(g_playerHouse))
+				Client_Send_SendStarportOrder(&s->o);
 			return true;
 		}
 	}
@@ -765,7 +706,7 @@ ActionPanel_DrawStarportOrder(const Widget *widget)
 
 	ActionPanel_SendOrderButtonDimensions(widget, &x1, &y1, &x2, &y2, &w, &h);
 
-	if (BuildQueue_IsEmpty(&g_playerHouse->starportQueue)) {
+	if (House_StarportQueueEmpty(g_playerHouse)) {
 		Prim_FillRect_RGBA(x1, y1, x2, y2, 0x9C, 0x9C, 0xB8, 0XFF);
 		Prim_DrawBorder(x1, y1, w, h, 1, true, false, 1);
 		fg = 0xE;
@@ -1034,7 +975,7 @@ ActionPanel_DrawFactory(const Widget *widget, Structure *s)
 		/* Draw build queue count. */
 		int count;
 		if (s->o.type == STRUCTURE_STARPORT) {
-			count = BuildQueue_Count(&g_playerHouse->starportQueue, object_type);
+			count = g_playerHouse->starportCount[object_type];
 		}
 		else {
 			count = BuildQueue_Count(&s->queue, object_type);

commit 8a8052583478cbe4a8234db585f6890d64f92cf9

Fix bug causing starport to only work once.
Bug introduced in commit 90c3ce757501466da3d55919611e256cee8bb880.

src/net/server.c
@@ -818,8 +818,11 @@ Server_Recv_SendStarportOrder(Structure *s)
 	if (BuildQueue_IsEmpty(&h->starportQueue))
 		return;
 
-	if (h->starportTimeLeft == 0) {
-		h->starportTimeLeft = g_table_houseInfo[h->index].starportDeliveryTime;
+	const uint16 delivery_time
+		= g_table_houseInfo[h->index].starportDeliveryTime;
+
+	if (h->starportTimeLeft == 0 || h->starportTimeLeft == delivery_time) {
+		h->starportTimeLeft = delivery_time;
 		memset(h->starportCount, 0, sizeof(h->starportCount));
 		BuildQueue_Free(&h->starportQueue);
 	}