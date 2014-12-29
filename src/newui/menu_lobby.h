761725c7e1502dc39b3a1cea79d06b5cbda029b8
Mathijs Visser <code@mathijsvisser.nl>
2014-12-29 21:53:14 +0100

    Add GUI for additional map options (thanks Mathijs).
    Fixes commit d2bf07abbb8e9d5cc9fc564483d22b02b964b846.

---
+++ src/newui/menu_lobby.h
@@ -0,0 +1,45 @@
+#ifndef NEWUI_MENU_LOBBY_H
+#define NEWUI_MENU_LOBBY_H
+
+#include "../gui/widget.h"
+
+enum MapOptions {
+	MAP_OPTIONS_WIDGET_SEED_RANDOM = 20,
+	MAP_OPTIONS_STARTING_CREDITS_MIN = 1000,
+	MAP_OPTIONS_STARTING_CREDITS_MAX = 10000,
+	MAP_OPTIONS_SPICE_MIN = 0,
+	MAP_OPTIONS_SPICE_MAX = 128,
+	MAP_OPTIONS_GUI_LINE_HEIGHT = 12,
+	MAP_OPTIONS_GUI_MAIN_X = 11,
+	MAP_OPTIONS_GUI_MAIN_Y = 83,
+	MAP_OPTIONS_GUI_MAIN_W = 110,
+	MAP_OPTIONS_GUI_MAIN_H = 90,
+	MAP_OPTIONS_GUI_MAP_X = 126,
+	MAP_OPTIONS_GUI_MAP_Y = MAP_OPTIONS_GUI_MAIN_Y,
+	MAP_OPTIONS_GUI_MAP_W = 183,
+	MAP_OPTIONS_GUI_MAP_H = 74,
+	MAP_OPTIONS_GUI_ERROR_X = 126,
+	MAP_OPTIONS_GUI_ERROR_Y = 164,
+	MAP_OPTIONS_GUI_ERROR_W = 183,
+	MAP_OPTIONS_GUI_ERROR_H = 9
+};
+
+extern void Lobby_InitWidgets(void);
+extern void Lobby_FreeWidgets(void);
+
+extern void PickLobby_Draw(void);
+extern enum MenuAction PickLobby_Loop(void);
+
+extern void MapOptionsLobby_Initialise(void);
+extern void MapOptionsLobby_Draw(void);
+extern enum MenuAction MapOptionsLobby_Loop(void);
+
+extern void SkirmishLobby_Initialise(void);
+extern void SkirmishLobby_Draw(void);
+extern enum MenuAction SkirmishLobby_Loop(void);
+
+extern void MultiplayerLobby_Initialise(void);
+extern void MultiplayerLobby_Draw(void);
+extern enum MenuAction MultiplayerLobby_Loop(void);
+
+#endif
