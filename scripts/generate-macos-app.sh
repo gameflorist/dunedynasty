73ffff5cf5320a10f1172f6a254028f7b33d94bc
bni <bnilsson@me.com>
2024-12-28 21:10:04 +0100

    Remove comment

--- scripts/generate-macos-app.sh
+++ scripts/generate-macos-app.sh
@@ -36,15 +36,6 @@ echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>
 dylibbundler -of -b -x $CONTENTS/MacOS/dunedynasty -d $CONTENTS/libs/
 otool -L $CONTENTS/MacOS/dunedynasty
 
-# Hack, fix for libfluidsynth needing SDL2 ???:
-# Termination Reason:    Namespace DYLD, Code 1 Library missing
-# Library not loaded: @rpath/SDL2.framework/Versions/A/SDL2
-# Referenced from: <xxx> /Users/USER/Documents/*/Dune Dynasty.app/Contents/libs/libfluidsynth.3.2.2.dylib
-# Reason: tried: '/Users/USER/Documents/dunedynasty/dist/Dune Dynasty.app/Contents/libs/SDL2.framework/Versions/A/SDL2' (no such file)
-# (terminated at launch; ignore backtrace)
-# Another way to fix is to disable compiling with libfluidsynth
-# cp -r /opt/local/lib/SDL2.framework $CONTENTS/libs/
-
 # Rename bundle with space in the name, as a final step.
 # dylibbundler has problems with paths containing spaces
 mv ../dist/DuneDynasty.app "../dist/Dune Dynasty.app"
