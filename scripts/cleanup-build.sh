48ea32bb97f6dc98fcd7462955d98d84c4be82c1
Gerald Buttinger <gerald@code.florist>
2025-03-08 15:11:29 +0100

    fix: use -f flag to force removal of build files in cleanup script

--- scripts/cleanup-build.sh
+++ scripts/cleanup-build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 rm -rf CMakeFiles
-rm CMakeCache.txt
-rm cmake_install.cmake
-rm Makefile
-rm include/buildcfg.h
+rm -f CMakeCache.txt
+rm -f cmake_install.cmake
+rm -f Makefile
+rm -f include/buildcfg.h
