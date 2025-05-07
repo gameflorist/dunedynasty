050139b31122216e92b7de4b73c0dc42faec51fc
Gerald Buttinger <gerald@code.florist>
2025-05-07 20:59:29 +0200

    ci: add workflow for windows arm64 build

---
+++ scripts/build-windows-arm64.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -e
+
+echo "Cleaning up build..."
+./scripts/cleanup-build.sh
+
+echo "Performing build..."
+cmake -G Ninja -DCMAKE_BUILD_TYPE=Release .
+ninja
+
+echo "Bundling dlls..."
+ldd ./dist/dunedynasty.exe | grep /clangarm64/bin |awk '{if(substr($3,0,1)=="/") print $1,$3}' |sort |cut -d\  -f2 |
+xargs -d '\n' -I{} cp --copy-contents {} ./dist
