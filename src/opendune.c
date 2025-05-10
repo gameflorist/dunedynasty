gameflorist/dunedynasty

commit ed63ed719d1700b31842b359906f6ab1a78f85dd

feat: add button to load skirmish savegame to menu

src/opendune.c
@@ -542,11 +542,13 @@ void GameLoop_Main(bool new_game)
 	Net_Synchronise();
 
 	if (new_game) {
-		Game_LoadScenario(g_playerHouseID, g_scenarioID);
-		GUI_ChangeSelectionType(g_debugScenario ? SELECTIONTYPE_DEBUG : SELECTIONTYPE_STRUCTURE);
-	} else if (g_campaign_selected == CAMPAIGNID_SKIRMISH
+		if (g_campaign_selected == CAMPAIGNID_SKIRMISH
 	        || g_campaign_selected == CAMPAIGNID_MULTIPLAYER) {
-		Skirmish_StartScenario();
+			Skirmish_StartScenario();
+		} else {
+			Game_LoadScenario(g_playerHouseID, g_scenarioID);
+			GUI_ChangeSelectionType(g_debugScenario ? SELECTIONTYPE_DEBUG : SELECTIONTYPE_STRUCTURE);
+		}
 	}
 
 	/* Note: original game chose only MUSIC_IDLE1 .. MUSIC_IDLE6. */

src/newui/menu.c
@@ -1036,14 +1036,42 @@ LoadGame_Draw(void)
 }
 
 static enum MenuAction
+PlaySkirmish_Loop(bool new_game)
+{
+	
+	do {
+		if (new_game && !Skirmish_GenerateMap(MAP_GENERATOR_FINAL))
+			return MENU_SKIRMISH_LOBBY;
+
+		PlayAGame_StartGame(new_game);
+	} while (g_gameMode == GM_RESTART);
+
+	if (g_gameMode == GM_WIN) {
+		Audio_PlayMusic(g_table_houseInfo[g_playerHouseID].musicWin);
+		return MENU_FADE_IN | MENU_SKIRMISH_SUMMARY;
+	} else if (g_gameMode == GM_LOSE) {
+		Audio_PlayMusic(g_table_houseInfo[g_playerHouseID].musicLose);
+		return MENU_FADE_IN | MENU_SKIRMISH_SUMMARY;
+	} else {
+		return MENU_SKIRMISH_LOBBY;
+	}
+}
+
+static enum MenuAction
 LoadGame_Loop(void)
 {
 	const int ret = SaveMenu_SaveLoad_Click(false);
 	bool redraw = false;
 
 	if (ret == -1) {
+		if (g_campaign_selected == CAMPAIGNID_SKIRMISH) {
+			return MENU_PICK_LOBBY;
+		}
 		return MENU_MAIN_MENU;
 	} else if (ret == -2) {
+		if (g_campaign_selected == CAMPAIGNID_SKIRMISH) {
+			return PlaySkirmish_Loop(false);
+		}
 		return PlayAGame_Loop(false);
 	} else if (ret == -3) {
 		redraw = true;
@@ -1061,27 +1089,6 @@ LoadGame_Loop(void)
 	}
 
 	return (redraw ? MENU_REDRAW : 0) | MENU_LOAD_GAME;
-}
-
-static enum MenuAction
-PlaySkirmish_Loop(void)
-{
-	do {
-		if (!Skirmish_GenerateMap(MAP_GENERATOR_FINAL))
-			return MENU_SKIRMISH_LOBBY;
-
-		PlayAGame_StartGame(false);
-	} while (g_gameMode == GM_RESTART);
-
-	if (g_gameMode == GM_WIN) {
-		Audio_PlayMusic(g_table_houseInfo[g_playerHouseID].musicWin);
-		return MENU_FADE_IN | MENU_SKIRMISH_SUMMARY;
-	} else if (g_gameMode == GM_LOSE) {
-		Audio_PlayMusic(g_table_houseInfo[g_playerHouseID].musicLose);
-		return MENU_FADE_IN | MENU_SKIRMISH_SUMMARY;
-	} else {
-		return MENU_SKIRMISH_LOBBY;
-	}
 }
 
 static enum MenuAction
@@ -1510,7 +1517,7 @@ Menu_Run(void)
 				break;
 
 			case MENU_PLAY_SKIRMISH:
-				res = PlaySkirmish_Loop();
+				res = PlaySkirmish_Loop(true);
 				break;
 
 			case MENU_PLAY_MULTIPLAYER:

src/newui/menu_lobby.c
@@ -13,6 +13,7 @@
 #include "editbox.h"
 #include "halloffame.h"
 #include "scrollbar.h"
+#include "savemenu.h"
 #include "../audio/audio.h"
 #include "../enhancement.h"
 #include "../gui/font.h"
@@ -77,9 +78,22 @@ PickLobby_InitWidgets(void)
 	w->flags.buttonFilterRight = 4;
 	pick_lobby_widgets = GUI_Widget_Link(pick_lobby_widgets, w);
 
-	/* Skirmish. */
+	/* Start Skirmish. */
 	width = Font_GetStringWidth(String_Get_ByIndex(STR_START)) + 20;
-	w = GUI_Widget_Allocate(10, 0, 11 + (102 - width) / 2, 148, 0xFFFE, STR_START);
+	w = GUI_Widget_Allocate(10, 0, 11 + (102 - width) / 2, 128, 0xFFFE, STR_START);
+	w->width  = width;
+	w->height = 12;
+	memset(&w->flags, 0, sizeof(w->flags));
+	w->flags.requiresClick = true;
+	w->flags.clickAsHover = true;
+	w->flags.loseSelect = true;
+	w->flags.buttonFilterLeft = 4;
+	w->flags.buttonFilterRight = 4;
+	pick_lobby_widgets = GUI_Widget_Link(pick_lobby_widgets, w);
+
+	/* Load Skirmish. */
+	width = Font_GetStringWidth(String_Get_ByIndex(STR_LOAD)) + 20;
+	w = GUI_Widget_Allocate(15, 0, 11 + (102 - width) / 2, 148, 0xFFFE, STR_LOAD);
 	w->width  = width;
 	w->height = 12;
 	memset(&w->flags, 0, sizeof(w->flags));
@@ -941,9 +955,6 @@ PickLobby_Draw(void)
 	Prim_DrawBorder( 11, 86, 102, 83, 1, false, false, 3);
 	GUI_DrawText_Wrapper("Skirmish", 61, 96, 0xF, 0, 0x122);
 
-	Shape_Draw(SHAPE_INFANTRY, 46, 118, WINDOWID_MAINMENU_FRAME, 0);
-	Prim_DrawBorder(46, 118, 32, 24, 1, false, false, 4);
-
 	/* Multiplayer. */
 	Prim_DrawBorder(120, 86, 189, 83, 1, false, false, 3);
 	GUI_DrawText("Multiplayer", 186, 96, 0xF, 0);
@@ -995,9 +1006,16 @@ PickLobby_Loop(void)
 			GUI_Widget_MakeNormal(GUI_Widget_Get_ByIndex(pick_lobby_widgets, 1), false);
 			return MENU_MAIN_MENU;
 
-		case 0x8000 | 10: /* skirmish. */
+		case 0x8000 | 10: /* start skirmish. */
 			GUI_Widget_MakeNormal(GUI_Widget_Get_ByIndex(pick_lobby_widgets, 10), false);
 			return MENU_NO_TRANSITION | MENU_SKIRMISH_LOBBY;
+
+		case 0x8000 | 15: /* load skirmish. */
+			GUI_Widget_MakeNormal(GUI_Widget_Get_ByIndex(pick_lobby_widgets, 15), false);
+			g_campaign_selected = CAMPAIGNID_SKIRMISH;
+			Campaign_Load();
+			SaveMenu_InitSaveLoad(false);
+			return MENU_NO_TRANSITION | MENU_LOAD_GAME;
 
 		case 0x8000 | 20: /* host */
 			GUI_Widget_MakeNormal(GUI_Widget_Get_ByIndex(pick_lobby_widgets, 20), false);

commit 9d013c0d0027be6a87dfd99612f92f617ebbf4d2

fix: broken multiplayer introduced by ed63ed719d1700b31842b359906f6ab1a78f85dd

src/opendune.c
@@ -542,13 +542,15 @@ void GameLoop_Main(bool new_game)
 	Net_Synchronise();
 
 	if (new_game) {
-		if (g_campaign_selected == CAMPAIGNID_SKIRMISH
-	        || g_campaign_selected == CAMPAIGNID_MULTIPLAYER) {
+		if (g_campaign_selected == CAMPAIGNID_SKIRMISH || g_campaign_selected == CAMPAIGNID_MULTIPLAYER) {
 			Skirmish_StartScenario();
 		} else {
 			Game_LoadScenario(g_playerHouseID, g_scenarioID);
 			GUI_ChangeSelectionType(g_debugScenario ? SELECTIONTYPE_DEBUG : SELECTIONTYPE_STRUCTURE);
 		}
+	}
+	else if (g_campaign_selected == CAMPAIGNID_MULTIPLAYER) {
+		Skirmish_StartScenario();
 	}
 
 	/* Note: original game chose only MUSIC_IDLE1 .. MUSIC_IDLE6. */